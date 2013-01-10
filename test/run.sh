d4a9cc7cc8c812da6dc1415e83bf3503e3506845
Thiago de Arruda <tpadilha84@gmail.com>
2013-01-10 08:27:14 -0300

    Improved test script so it will work on osx

--- test/run.sh
+++ test/run.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 if [ ! -x ./node_modules/.bin/phantomjs ]; then
-        f='phantomjs-1.7.0-linux-i686.tar.bz2'
-        d="${f%.tar.bz2}"
-        wget http://phantomjs.googlecode.com/files/$f
-        tar xf $f
+        case $OSTYPE in
+                darwin*)
+                        f=phantomjs-1.8.1-macosx.zip
+                        d="${f%.zip}"
+                        wget http://phantomjs.googlecode.com/files/$f
+                        unzip $f
+                        ;;
+                *)
+                        f=phantomjs-1.8.1-linux-i686.tar.bz2
+                        d="${f%.tar.bz2}"
+                        wget http://phantomjs.googlecode.com/files/$f
+                        tar xf $f
+                        ;;
+        esac
         mv "$d/bin/phantomjs" ./node_modules/.bin/
         rm -rf "$d"
         rm "$f"
