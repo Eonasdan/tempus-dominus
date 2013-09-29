223c3923d367f8e1a3d8a2b44cc42919eb320bd5
Nick Baugh <niftylettuce@gmail.com>
2013-09-29 16:31:47 -0400

    this.component and icon errors fixed for lack of input-group and addon

--- src/js/bootstrap-datetimepicker.js
+++ src/js/bootstrap-datetimepicker.js
@@ -10,6 +10,7 @@
  *  - Andrew Rowls
  *  - Thiago de Arruda
  *  - updated for Bootstrap v3 by Jonathan Peterson @Eonasdan
+ *  - Nick Baugh
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,105 +29,107 @@
 (function ($) {
 
     // Picker object
-    var smartPhone = (window.orientation != undefined);
+    var smartPhone = (window.orientation !== undefined);
     var DateTimePicker = function (element, options) {
-        this.id = dpgId++;
-        this.init(element, options);
-    };
+      this.id = dpgId++;
+      this.init(element, options);
+    }
 
     var dateToDate = function (dt) {
-        if (typeof dt === 'string') {
-            return new Date(dt);
-        }
-        return dt;
-    };
+      if (typeof dt === 'string') {
+        return new Date(dt);
+      }
+      return dt;
+    }
 
     DateTimePicker.prototype = {
         constructor: DateTimePicker,
 
         init: function (element, options) {
-            var icon;
-            if (!(options.pickTime || options.pickDate))
-                throw new Error('Must choose at least one picker');
-            this.options = options;
-            this.$element = $(element);
-            this.language = options.language in dates ? options.language : 'en';
-            this.pickDate = options.pickDate;
-            this.pickTime = options.pickTime;
-            this.isInput = this.$element.is('input');
-            this.component = false;
-            if (this.$element.find('.input-group'))
-                this.component = this.$element.find('.input-group-addon');
-            this.format = options.format;
-            if (!this.format) {
-                if (this.isInput) this.format = this.$element.data('format');
-                else this.format = this.$element.find('input').data('format');
-                if (!this.format) this.format = 'MM/dd/yyyy' + (this.pickTime ? ' hh:mm' : '') + (this.pickSeconds ? ':ss' : '');
+          var icon = false
+          if (!(options.pickTime || options.pickDate))
+            throw new Error('Must choose at least one picker');
+          this.options = options;
+          this.$element = $(element);
+          this.language = options.language in dates ? options.language : 'en';
+          this.pickDate = options.pickDate;
+          this.pickTime = options.pickTime;
+          this.isInput = this.$element.is('input');
+          this.component = false;
+          if (this.$element.find('.input-group').length)
+            this.component = this.$element.find('.input-group-addon');
+          this.format = options.format;
+          if (!this.format) {
+            if (this.isInput) this.format = this.$element.data('format');
+            else this.format = this.$element.find('input').data('format');
+            if (!this.format) this.format = 'MM/dd/yyyy' + (this.pickTime ? ' hh:mm' : '') + (this.pickSeconds ? ':ss' : '');
+          }
+          this._compileFormat();
+          if (this.component)
+            icon = this.component.find('span');
+          if (this.pickTime) {
+            if (icon && icon.length) {
+              this.timeIcon = icon.data('time-icon');
+              this.upIcon = icon.data('up-icon');
+              this.downIcon = icon.data('down-icon');
             }
-            this._compileFormat();
-            if (this.component) {
-                icon = this.component.find('span');
+            if (!this.timeIcon) this.timeIcon = 'glyphicon glyphicon-time';
+            if (!this.upIcon) this.upIcon = 'glyphicon glyphicon-chevron-up';
+            if (!this.downIcon) this.downIcon = 'glyphicon glyphicon-chevron-down';
+            if (icon)
+              icon.addClass(this.timeIcon);
+          }
+          if (this.pickDate) {
+            if (icon && icon.length) this.dateIcon = icon.data('date-icon');
+            if (!this.dateIcon) this.dateIcon = 'glyphicon glyphicon-calendar';
+            if (icon) {
+              icon.removeClass(this.timeIcon);
+              icon.addClass(this.dateIcon);
             }
-            if (this.pickTime) {
-                if (icon && icon.length) {
-                  this.timeIcon = icon.data('time-icon');
-                  this.upIcon = icon.data('up-icon');
-                  this.downIcon = icon.data('down-icon');
-                }
-                if (!this.timeIcon) this.timeIcon = 'glyphicon glyphicon-time';
-                if (!this.upIcon) this.upIcon = 'glyphicon glyphicon-chevron-up';
-                if (!this.downIcon) this.downIcon = 'glyphicon glyphicon-chevron-down';
-                icon.addClass(this.timeIcon);
+          }
+          this.widget = $(getTemplate(this.timeIcon, this.upIcon, this.downIcon, options.pickDate, options.pickTime,
+                                      options.pick12HourFormat, options.pickSeconds, options.collapse)).appendTo('body');
+          this.minViewMode = options.minViewMode || this.$element.data('date-minviewmode') || 0;
+          if (typeof this.minViewMode === 'string') {
+            switch (this.minViewMode) {
+            case 'months':
+              this.minViewMode = 1;
+              break;
+            case 'years':
+              this.minViewMode = 2;
+              break;
+            default:
+              this.minViewMode = 0;
+              break;
             }
-            if (this.pickDate) {
-                if (icon && icon.length) this.dateIcon = icon.data('date-icon');
-                if (!this.dateIcon) this.dateIcon = 'glyphicon glyphicon-calendar';
-                icon.removeClass(this.timeIcon);
-                icon.addClass(this.dateIcon);
+          }
+          this.viewMode = options.viewMode || this.$element.data('date-viewmode') || 0;
+          if (typeof this.viewMode === 'string') {
+            switch (this.viewMode) {
+            case 'months':
+              this.viewMode = 1;
+              break;
+            case 'years':
+              this.viewMode = 2;
+              break;
+            default:
+              this.viewMode = 0;
+              break;
             }
-            this.widget = $(getTemplate(this.timeIcon, this.upIcon, this.downIcon, options.pickDate, options.pickTime,
-                                        options.pick12HourFormat, options.pickSeconds, options.collapse)).appendTo('body');
-            this.minViewMode = options.minViewMode || this.$element.data('date-minviewmode') || 0;
-            if (typeof this.minViewMode === 'string') {
-                switch (this.minViewMode) {
-                    case 'months':
-                        this.minViewMode = 1;
-                        break;
-                    case 'years':
-                        this.minViewMode = 2;
-                        break;
-                    default:
-                        this.minViewMode = 0;
-                        break;
-                }
-            }
-            this.viewMode = options.viewMode || this.$element.data('date-viewmode') || 0;
-            if (typeof this.viewMode === 'string') {
-                switch (this.viewMode) {
-                    case 'months':
-                        this.viewMode = 1;
-                        break;
-                    case 'years':
-                        this.viewMode = 2;
-                        break;
-                    default:
-                        this.viewMode = 0;
-                        break;
-                }
-            }
-            this.startViewMode = this.viewMode;
-            this.weekStart = options.weekStart || this.$element.data('date-weekstart') || 0;
-            this.weekEnd = this.weekStart === 0 ? 6 : this.weekStart - 1;
-            this.setStartDate(options.startDate || this.$element.data('date-startdate'));
-            this.setEndDate(options.endDate || this.$element.data('date-enddate'));
-            this.fillDow();
-            this.fillMonths();
-            this.fillHours();
-            this.fillMinutes();
-            this.fillSeconds();
-            this.update();
-            this.showMode();
-            this._attachDatePickerEvents();
+          }
+          this.startViewMode = this.viewMode;
+          this.weekStart = options.weekStart || this.$element.data('date-weekstart') || 0;
+          this.weekEnd = this.weekStart === 0 ? 6 : this.weekStart - 1;
+          this.setStartDate(options.startDate || this.$element.data('date-startdate'));
+          this.setEndDate(options.endDate || this.$element.data('date-enddate'));
+          this.fillDow();
+          this.fillMonths();
+          this.fillHours();
+          this.fillMinutes();
+          this.fillSeconds();
+          this.update();
+          this.showMode();
+          this._attachDatePickerEvents();
         },
 
         show: function (e) {
@@ -270,6 +273,7 @@
         place: function () {
             var position = 'absolute';
             var offset = this.component ? this.component.offset() : this.$element.offset();
+            console.log('this.component', this.component)
             this.width = this.component ? this.component.outerWidth() : this.$element.outerWidth();
             offset.top = offset.top + this.height;
 
