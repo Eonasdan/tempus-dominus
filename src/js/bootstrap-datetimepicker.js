0d66c2d80302b227303f3f5852e0011ada13a61e
Nikos Kalogridis <nikos.kalogridis@webrules.gr>
2014-09-09 13:50:41 +0300

    further css cleanup

--- src/js/bootstrap-datetimepicker.js
+++ src/js/bootstrap-datetimepicker.js
@@ -221,7 +221,6 @@
                             .append($('<table>').addClass('table-condensed')),
                         ret = [getTimePickerMainTemplate()];
 
-
                     if (isEnabled('h')) {
                         ret.push(hoursView);
                     }
