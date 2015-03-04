45bd3c2297c647e060bd95c867bdfc30a4807a58
thomas <thomas.schiela@gmail.com>
2015-03-04 15:41:20 +0100

    fix #848, added option focusOnShow for disable auto focusing input field

--- src/js/bootstrap-datetimepicker.js
+++ src/js/bootstrap-datetimepicker.js
@@ -1020,7 +1020,7 @@
                 widget.show();
                 place();
 
-                if (!input.is(':focus')) {
+                if (options.focusOnShow && !input.is(':focus')) {
                     input.focus();
                 }
 
@@ -1764,6 +1764,19 @@
             return picker;
         };
 
+        picker.focusOnShow = function (focusOnShow) {
+            if (arguments.length === 0) {
+                return options.focusOnShow;
+            }
+
+            if (typeof focusOnShow !== 'boolean') {
+                throw new TypeError('focusOnShow() expects a boolean parameter');
+            }
+
+            options.focusOnShow = focusOnShow;
+            return picker;
+        };
+
         picker.inline = function (inline) {
             if (arguments.length === 0) {
                 return options.inline;
@@ -1939,6 +1952,7 @@
         widgetParent: null,
         ignoreReadonly: false,
         keepOpen: false,
+        focusOnShow: true,
         inline: false,
         keepInvalid: false,
         datepickerInput: '.datepickerinput',
