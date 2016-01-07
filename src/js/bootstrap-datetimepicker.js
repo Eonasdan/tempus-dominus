3eaac5616f19565817626a93d9e1f5cd7dc14b3e
Benjamin Roedell <benrobot@aol.com>
2016-01-06 19:45:37 -0500

    Correct code style errors.

--- src/js/bootstrap-datetimepicker.js
+++ src/js/bootstrap-datetimepicker.js
@@ -883,12 +883,12 @@
                     if (!options.keepInvalid) {
                         input.val(unset ? '' : date.format(actualFormat));
                     } else {
-						notifyEvent({
-							type: 'dp.change',
-							date: targetMoment,
-							oldDate: oldDate
-						});
-					}
+                        notifyEvent({
+                            type: 'dp.change',
+                            date: targetMoment,
+                            oldDate: oldDate
+                        });
+                    }
                     notifyEvent({
                         type: 'dp.error',
                         date: targetMoment
