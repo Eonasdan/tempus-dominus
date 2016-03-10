d0f30f8005b4e3de1ebec5d634cbab3380dd2053
Lydon Gray <lgray@avhodom1.com>
2016-03-10 16:38:24 +1100

    Fixed issue 1505 - widget positioning itself relative to top of parent, rather than bottom of parent.

--- src/js/bootstrap-datetimepicker.js
+++ src/js/bootstrap-datetimepicker.js
@@ -461,8 +461,8 @@
                 }
 
                 widget.css({
-                    top: vertical === 'top' ? 'auto' : position.top + element.outerHeight(),
-                    bottom: vertical === 'top' ? parent.outerHeight() - (parent === element ? 0 : position.top) : 'auto',
+                    top: vertical === 'top' ? position.top - widget.height() - element.outerHeight() / 2 : position.top + element.outerHeight(),
+                    bottom: 'auto',
                     left: horizontal === 'left' ? (parent === element ? 0 : position.left) : 'auto',
                     right: horizontal === 'left' ? 'auto' : parent.outerWidth() - element.outerWidth() - (parent === element ? 0 : position.left)
                 });
