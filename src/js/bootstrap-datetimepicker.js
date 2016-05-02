9d1a29e5ee2aa965cc24c4fe6ca42ab4a59be328
Tomáš Pažourek <tom.pazourek@gmail.com>
2016-05-02 18:41:43 +0800

    Fixed problem with DST

--- src/js/bootstrap-datetimepicker.js
+++ src/js/bootstrap-datetimepicker.js
@@ -151,10 +151,10 @@
                     }
                 } else {
                     if (tzEnabled) {
-                        currentZoneOffset = moment().tz(options.timeZone).utcOffset();
+                        currentZoneOffset = moment(d, parseFormats, options.useStrict).tz(options.timeZone).utcOffset();
                         incomingZoneOffset = moment(d, parseFormats, options.useStrict).utcOffset();
                         if (incomingZoneOffset !== currentZoneOffset) {
-                            timeZoneIndicator = moment().tz(options.timeZone).format('Z');
+                            timeZoneIndicator = moment(d, parseFormats, options.useStrict).tz(options.timeZone).format('Z');
                             dateWithTimeZoneInfo = moment(d, parseFormats, options.useStrict).format('YYYY-MM-DD[T]HH:mm:ss') + timeZoneIndicator;
                             returnMoment = moment(dateWithTimeZoneInfo, parseFormats, options.useStrict).tz(options.timeZone);
                         } else {
