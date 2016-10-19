fba1e544c7b404f0e3dc4873b13f9e12fd657464
Eonasdan <jonpet2@gmail.com>
2016-10-19 10:56:11 -0400

    bumpb version

--- src/js/bootstrap-datetimepicker.js
+++ src/js/bootstrap-datetimepicker.js
@@ -1,4 +1,4 @@
-/*! version : 4.17.42
+/*! version : 4.17.43
  =========================================================
  bootstrap-datetimejs
  https://github.com/Eonasdan/bootstrap-datetimepicker
