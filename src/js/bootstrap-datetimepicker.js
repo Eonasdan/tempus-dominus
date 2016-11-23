e60b04ea7fb44dd0990b1cd6191be2ad7569f7ad
Trask Stalnaker <trask.stalnaker@gmail.com>
2016-11-22 20:15:36 -0800

    Fix time is reset to current when date is changed

--- src/js/bootstrap-datetimepicker.js
+++ src/js/bootstrap-datetimepicker.js
@@ -866,7 +866,7 @@
 
                 if (isValid(targetMoment)) {
                     date = targetMoment;
-                    //viewDate = date.clone(); // TODO this doesn't work right on first use
+                    viewDate = date.clone(); // TODO this doesn't work right on first use
                     input.val(date.format(actualFormat));
                     element.data('date', date.format(actualFormat));
                     unset = false;
