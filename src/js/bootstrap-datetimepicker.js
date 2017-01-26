1815e95d4b2c9353f6018d359990ed149fb14fb7
Eonasdan <jonpet2@gmail.com>
2017-01-26 09:01:09 -0500

    can we please get this fixed now

--- src/js/bootstrap-datetimepicker.js
+++ src/js/bootstrap-datetimepicker.js
@@ -1,4 +1,4 @@
-/*! version : 4.17.45
+/*! version : 4.17.46
  =========================================================
  bootstrap-datetimejs
  https://github.com/Eonasdan/bootstrap-datetimepicker
