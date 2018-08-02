4aa059a059ce054edbb85919cb2ec933546164ba
Eonasdan <me@eonasdan.com>
2018-08-02 18:22:14 -0400

    docs

--- docs/theme/js/base.js
+++ docs/theme/js/base.js
@@ -1,6 +1,6 @@
 
 /* Prettyify */
-$( document ).ready(function() {
+$(document).ready(function () {
     prettyPrint();
 });
 
@@ -15,14 +15,14 @@ $('body').scrollspy({
 
 
 /* Prevent disabled links from causing a page reload */
-$("li.disabled a").click(function() {
+$("li.disabled a").click(function () {
     event.preventDefault();
 });
 
 
 /* Adjust the scroll height of anchors to compensate for the fixed navbar */
 window.disableShift = false;
-var shiftWindow = function() {
+var shiftWindow = function () {
     if (window.disableShift) {
         window.disableShift = false;
     } else {
@@ -35,18 +35,29 @@ var shiftWindow = function() {
         };
     };
 };
-if (location.hash) {shiftWindow();}
+if (location.hash) { shiftWindow(); }
 window.addEventListener("hashchange", shiftWindow);
 
 
 /* Deal with clicks on nav links that do not change the current anchor link. */
-$("ul.nav a" ).click(function() {
+$("ul.nav a").click(function () {
     var href = this.href;
     var suffix = location.hash;
     var matchesCurrentHash = (href.indexOf(suffix, href.length - suffix.length) !== -1);
     if (location.hash && matchesCurrentHash) {
         /* Force a single 'hashchange' event to occur after the click event */
         window.disableShift = true;
-        location.hash='';
+        location.hash = '';
     };
 });
+
+$(document).bind('DOMNodeInserted',
+    function (e) {
+        if (e.target.id === 'carbonads') {
+            $('.bs-sidebar').affix({
+                offset: {
+                    top: $('#carbonads').height() + 10
+                }
+            });
+        }
+    });
