544a158c553cbf63e9a17c6bf171f700d448f1c4
Eonasdan <me@eonasdan.com>
2022-05-26 08:50:32 -0400

    * added locale from pr * updated github templates * updated link nav

---
+++ src/locales/fi.ts
@@ -0,0 +1,35 @@
+const name = 'de';
+
+const localization = {
+  today: 'Tänään',
+  clear: 'Tyhjennä',
+  close: 'Sulje',
+  selectMonth: 'Valitse kuukausi',
+  previousMonth: 'Edellinen kuukausi',
+  nextMonth: 'Seuraava kuukausi',
+  selectYear: 'Valitse vuosi',
+  previousYear: 'Edelline vuosi',
+  nextYear: 'Seuraava vuosi',
+  selectDecade: 'Valitse vuosikymmen',
+  previousDecade: 'Edellinen vuosikymmen',
+  nextDecade: 'Seuraava vuosikymmen',
+  previousCentury: 'Edellinen vuosisata',
+  nextCentury: 'Seuraava vuosisata',
+  pickHour: 'Valitse tunnit',
+  incrementHour: 'Vähennä tunteja',
+  decrementHour: 'Lisää tunteja',
+  pickMinute: 'Valitse minuutit',
+  incrementMinute: 'Vähennä minuutteja',
+  decrementMinute: 'Lisää minuutteja',
+  pickSecond: 'Valitse sekuntit',
+  incrementSecond: 'Vähennä sekunteja',
+  decrementSecond: 'Lisää sekunteja',
+  toggleMeridiem: 'Vaihda kellonaikaa',
+  selectTime: 'Valitse aika',
+  selectDate: 'Valise päivä',
+  dayViewHeaderFormat: { month: 'long', year: '2-digit' },
+  locale: 'fi',
+  startOfTheWeek: 1
+};
+
+export { localization, name };
