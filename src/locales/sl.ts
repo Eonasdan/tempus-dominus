b9e6f86f4bbfeace24c74cc85b9de2b902af27eb
Bojan Mihelac <bmihelac@mihelac.org>
2022-07-14 15:51:11 +0200

    feat: add slovenian localization (#2606)

---
+++ src/locales/sl.ts
@@ -0,0 +1,35 @@
+const name = 'sl';
+
+const localization = {
+  today: 'Danes',
+  clear: 'Počisti',
+  close: 'Zapri',
+  selectMonth: 'Izberite mesec',
+  previousMonth: 'Prejšnji mesec',
+  nextMonth: 'Naslednji mesec',
+  selectYear: 'Izberite leto',
+  previousYear: 'Prejšnje Leto',
+  nextYear: 'Naslednje leto',
+  selectDecade: 'Izberite desetletje',
+  previousDecade: 'Prejšnje desetletje',
+  nextDecade: 'Naslednje desetletje',
+  previousCentury: 'Prejšnje stoletje',
+  nextCentury: 'Naslednje stoletje',
+  pickHour: 'Izberite uro',
+  incrementHour: 'Povečaj ure',
+  decrementHour: 'Zmanjšaj uro',
+  pickMinute: 'Izberite minuto',
+  incrementMinute: 'Povečaj minuto',
+  decrementMinute: 'Zmanjšaj minuto',
+  pickSecond: 'Izberite drugo',
+  incrementSecond: 'Povečaj sekundo',
+  decrementSecond: 'Zmanjšaj sekundo',
+  toggleMeridiem: 'Preklop dopoldne/popoldne',
+  selectTime: 'Izberite čas',
+  selectDate: 'Izberite Datum',
+  dayViewHeaderFormat: { month: 'long', year: 'numeric' },
+  locale: 'sl',
+  startOfTheWeek: 1,
+};
+
+export { localization, name };
