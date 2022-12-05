42a12cd75f9b901ecc68eb8c08badc4672a8f060
Eonasdan <me@eonasdan.com>
2022-12-05 14:50:43 -0500

    updating vite config and remove generated coverage from repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import GithubActionsReporter from 'vitest-github-actions-reporter';
 export default defineConfig({
   test: {
     coverage: {
-      reporter: ['text', 'json', 'html'],
+      reporter: ['text', 'json', 'html', 'lcovonly'],
+      exclude: ['**/*.test.ts'],
     },
     reporters: process.env.GITHUB_ACTIONS
       ? ['default', new GithubActionsReporter()]
