896b44c369b2be707b80c9395deb6ca7f91a9107
Jonathan Peterson <me@eonasdan.com>
2022-12-12 15:50:09 -0500

    rollback

--- types/plugins/customDateFormat/index.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: (_: any, tdClasses: any) => void;
-export default _default;
--- types/plugins/examples/sample.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: (option: any, tdClasses: any, tdFactory: any) => void;
-export default _default;
