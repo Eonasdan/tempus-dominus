081364d632cab56facb03ddf55f144f36933c7e3
John Pariseau <simplymail@ururk.com>
2023-03-15 16:08:13 -0500

    Add Bootstrap Icons plugin

---
+++ types/plugins/bi-one.d.ts
@@ -0,0 +1,14 @@
+declare const biOneIcons: {
+    type: string;
+    time: string;
+    date: string;
+    up: string;
+    down: string;
+    previous: string;
+    next: string;
+    today: string;
+    clear: string;
+    close: string;
+};
+declare const load: (_: any, __: any, tdFactory: any) => void;
+export { biOneIcons, load };
