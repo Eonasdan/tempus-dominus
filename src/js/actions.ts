9f3dcd2194b8c35d72276f9021065887c2045bbb
iTzVirtual_ <58611844+iTzVirtual00@users.noreply.github.com>
2024-09-07 21:44:27 +0200

    Fix #2931 (#2932)
    * Fix #2931
    
    Fixes the daterange freeze that happens when clicking "Go to today" after selecting a date range that starts after today's date
    
    * Adds back the validation check before setting the date

--- src/js/actions.ts
+++ src/js/actions.ts
@@ -143,14 +143,20 @@ export default class Actions {
         this.display.hide();
         break;
       case ActionTypes.today: {
-        const today = new DateTime().setLocalization(
-          this.optionsStore.options.localization
+        const day = new DateTime().setLocalization(
+            this.optionsStore.options.localization
         );
-        this._eventEmitters.updateViewDate.emit(today);
+        this._eventEmitters.updateViewDate.emit(day);
 
-        //todo this this really a good idea?
-        if (this.validation.isValid(today, Unit.date))
-          this.dates.setValue(today, this.dates.lastPickedIndex);
+        if (!this.validation.isValid(day, Unit.date)) break;
+
+        if (this.optionsStore.options.dateRange)
+            this.handleDateRange(day);
+        else if (this.optionsStore.options.multipleDates) {
+            this.handleMultiDate(day);
+        } else {
+            this.dates.setValue(day, this.dates.lastPickedIndex);
+        }
         break;
       }
     }
